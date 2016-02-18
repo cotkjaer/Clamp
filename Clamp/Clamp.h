a0a9b50f424f0cb2e11f47b840d632e512d790bd
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 15:23:21 +0100

    Initial Commit

---
+++ Clamp/Clamp.h
@@ -0,0 +1,19 @@
+//
+//  Clamp.h
+//  Clamp
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Clamp.
+FOUNDATION_EXPORT double ClampVersionNumber;
+
+//! Project version string for Clamp.
+FOUNDATION_EXPORT const unsigned char ClampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clamp/PublicHeader.h>
+
+
