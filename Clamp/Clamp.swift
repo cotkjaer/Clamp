de90ba3d81e9949534308487f2e3ed43a17f333d
christian otkjær <christian.otkjaer@gmail.com>
2016-03-01 10:25:12 +0100

    Added Optionals

--- Clamp/Clamp.swift
+++ Clamp/Clamp.swift
@@ -8,23 +8,79 @@
 
 // MARK: - Comparable
 
-public func clamp<T: Comparable>(value: T, lower: T, upper: T) -> T
+/**
+`value` clamped to a given range.
+
+- parameter value: (optional) value to be clamped
+- parameter lower: minimum value
+- parameter upper: maximum value
+
+- returns: `value` clamped to the given range; `[lower;upper]`
+*/
+@warn_unused_result
+public func clamp<T: Comparable>(value: T?, lower: T? = nil, upper: T? = nil) -> T?
 {
-    return value.clamped(lower, upper)
+    return value?.clamped(lower: lower, upper: upper)
 }
+
+/**
+ `value` clamped to a given range.
+ 
+ - parameter value: (optional) value to be clamped
+ - parameter range: the range to clamp to
+ 
+ - returns: `value` clamped to the given range; `[lower;upper]`
+ */
+@warn_unused_result
+public func clamp<T: Comparable>(value: T?, range: Range<T>?) -> T?
+{
+    return value?.clamped(range)
+}
+
 
 public extension Comparable
 {
+    /**
+     `self` clamped to a given range.
+     
+     - parameter lower: minimum value
+     - parameter upper: maximum value
+     
+     - returns: `self` clamped to the given range; `[lower;upper]`
+     */
+    @warn_unused_result
     func clamped(lower: Self, _ upper: Self) -> Self
     {
         guard upper >= lower else { return self }
         
         if self > upper { return upper }
+        
         if self < lower { return lower }
+        
         return self
-//        return min(max(self, lower), upper)
     }
     
+    func clamped(lower lower: Self? = nil, upper: Self? = nil) -> Self
+    {
+        if let upper = upper
+        {
+            if self > upper { return upper }
+        }
+        
+        if let lower = lower
+        {
+            if self < lower { return lower }
+        }
+        
+        return self
+    }
+    
+    /**
+     Clamps `self` to a given range; `[lower;upper]`
+     
+     - parameter lower: minimum value
+     - parameter upper: maximum value
+     */
     mutating func clamp(lower: Self, _ upper: Self)
     {
         self = clamped(lower, upper)
@@ -36,10 +92,11 @@ public extension Comparable
 extension ForwardIndexType where Self : Comparable
 {
     /**
-     Clamps self to a specified range.
+     `self` clamped to a given range.
      
-     - parameter range: Clamping range
-     - returns: Clamped value
+     - parameter range: (optional) clamping range
+     
+     - returns: `self` clamped to the given range; `[startIndex;endIndex[` or `self` if range is `nil`
      */
     func clamped(range: Range<Self>?) -> Self
     {
@@ -51,6 +108,11 @@ extension ForwardIndexType where Self : Comparable
         return self
     }
 
+    /**
+     Clamps `self` to a specified range; `[startIndex;endIndex[`
+     
+     - parameter range: (optional) clamping range
+     */
     mutating func clamp(range: Range<Self>?)
     {
         self = clamped(range)
@@ -61,13 +123,17 @@ extension ForwardIndexType where Self : Comparable
 
 extension Range where Element : Comparable
 {
+    /**
+     Clamps `index` to `self`; `[startIndex;endIndex[`
+     
+     - parameter index: (optional) index to be clamped
+     
+     - returns: an index that is within `self`
+     */
+    @warn_unused_result
     public func clamp(index: Element?) -> Element?
     {
         return index?.clamped(self)
     }
 }
 
-public func clamp<T: Comparable>(value: T?, range: Range<T>?) -> T?
-{
-    return value?.clamped(range)//range.startIndex, range.endIndex.advancedBy(-1))
-}
--- ClampTests/ClampTests.swift
+++ ClampTests/ClampTests.swift
@@ -39,13 +39,91 @@ class ClampTests: XCTestCase
         XCTAssertEqual(double.clamped(-10, 1), 1)
     }
     
+
+    func test_clamped_optionals()
+    {
+        var double : Double? = nil
+        
+        XCTAssertNil(double?.clamped())
+
+        XCTAssertNil(double?.clamped(1, 2))
+
+        XCTAssertNil(double?.clamped(lower: nil, upper: nil))
+        
+        double = 3
+        
+        XCTAssertEqual(double?.clamped(2, 5), 3)
+        
+        XCTAssertEqual(double?.clamped(5, 7), 5)
+        
+        XCTAssertEqual(double?.clamped(-10, 1), 1)
+        
+        XCTAssertEqual(double?.clamped(lower: 5), 5)
+
+        
+        XCTAssertEqual(double?.clamped(upper: 5), 3)
+
+        var upper : Double? = 4
+        var lower : Double? = 4
+
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), 4)
+        
+        upper = 10
+        lower = 5
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), lower)
+        
+        upper = 1
+        lower = 0
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), upper)
+        
+        upper = nil
+        lower = nil
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), double)
+        
+        lower = 2
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), double)
+        
+        lower = 4
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), lower)
+        
+        lower = nil
+        upper = 4
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), double)
+        
+        upper = 2
+        
+        XCTAssertEqual(double?.clamped(lower: lower, upper: upper), upper)
+    }
+    
+
     func test_static_clamp()
     {
-        let float : Float = 4
+        var float : Float? = nil
+        
+        XCTAssertNil(clamp(float, lower: nil, upper: nil))
+
+        XCTAssertNil(clamp(float, lower: 1, upper: nil))
+        
+        XCTAssertNil(clamp(float, lower: nil, upper: 1))
+        
+        XCTAssertNil(clamp(float, lower: 1, upper: 1))
+
+        float = 4
         
         XCTAssertEqual(clamp(float, lower: 2, upper: 3), 3)
         
-        XCTAssertEqual(clamp(float, lower: 5, upper: 10), 5)
+        XCTAssertEqual(clamp(float, lower: 2, upper: nil), float)
+
+        XCTAssertEqual(clamp(float, lower: nil, upper: nil), float)
+        
+        XCTAssertEqual(clamp(float, lower: 5, upper: nil), 5)
+        XCTAssertEqual(clamp(float, lower: nil, upper: 10), float)
     }
     
     func test_range()
